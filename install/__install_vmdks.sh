415e7c70cb7cfc9b99a3ce91a8bf07397acdf5eb
Jeff McAffee <jeff@ktechsystems.com>
2012-08-22 11:41:59 -0700

    Adding support for choosing which VMDKs to install

--- install/__install_vmdks.sh
+++ install/__install_vmdks.sh
@@ -8,27 +8,85 @@
 # Website::   http://ktechsystems.com
 ##############################################################################
 
-echo -n "Do you want to download VMDKs (Y/n: default: n)? "
-read do_download
+##############################################
+# Functions
+#
 
-if [[ "x$do_download" != "xY" ]]; then
+get_vmdk_choice () {
+    local choice="rails"
+    echo "Select a VMDK to download:"
+    for vm in ${vmlist[@]} ; do
+        echo "    $vm"
+    done
+    echo "    all"
+    echo ""
+    echo -n "Choice (default: rails)? "
+    read choice
+
+    if [[ "x$choice" != "x" ]]; then
+        VMDK=$choice
+    else
+        VMDK=rails
+    fi
+}
+
+get_skip_downloads_choice () {
+    local do_download="n"
+    echo -n "Do you want to download VMDKs (Y/n: default: n)? "
+    read do_download
+
+    if [[ "x$do_download" != "xY" ]]; then
+        SKIP_DOWNLOADS="Y"
+        return
+    fi
+
+    SKIP_DOWNLOADS="N"
+}
+
+##############################################
+# Variables
+#
+
+vmlist=(core rails ec2sdk mysql)
+vmzips=(turnkey-core-12.0rc2-squeeze-x86-vmdk.zip turnkey-rails-11.3-lucid-x86-vmdk.zip turnkey-ec2sdk-11.3-lucid-x86-vmdk.zip turnkey-mysql-11.3-lucid-x86-vmdk.zip)
+turnkey="http://downloads.sourceforge.net/project/turnkeylinux/vmdk"
+
+
+##############################################
+# User Input
+#
+
+get_skip_downloads_choice
+if [[ "x$SKIP_DOWNLOADS" == "xY" ]]; then
     echo "Skipping VMDK downloads."
     exit 0;
 fi
+echo
+
+get_vmdk_choice
+echo
+echo
 
 ##############################################
 # Download some handy VMDKs
 #
 
-
+echo "- - - Downloading VMDKs - - -"
+echo
 vmtools_avail_vms=/opt/vmtools/avail-vms
 mkdir -p $vmtools_avail_vms
 
 cd $vmtools_avail_vms
-wget http://downloads.sourceforge.net/project/turnkeylinux/vmdk/turnkey-rails-11.3-lucid-x86-vmdk.zip
-wget http://downloads.sourceforge.net/project/turnkeylinux/vmdk/turnkey-core-12.0rc2-squeeze-x86-vmdk.zip
-wget http://downloads.sourceforge.net/project/turnkeylinux/vmdk/turnkey-ec2sdk-11.3-lucid-x86-vmdk.zip
-wget http://downloads.sourceforge.net/project/turnkeylinux/vmdk/turnkey-mysql-11.3-lucid-x86-vmdk.zip
+
+index=0
+for vm in ${vmlist[@]} ; do
+    if [[ "X$VMDK" == "Xall" ]] || [[ "X$VMDK" == "X$vm" ]]; then
+        echo "Downloading ${vmlist[$index]} vmdk..."
+        wget $turnkey/${vmzips[$index]}
+    fi
+    let index=index+1
+done
+
 
 cd -
 exit 0;
