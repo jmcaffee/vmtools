58c8e85c1be680a609f007ee5becd92fe9e36b4c
Jeff McAffee <jeff@ktechsystems.com>
2012-08-21 19:05:35 -0700

    Correcting error copying scripts to /bin/bin

--- install/__install_scripts.sh
+++ install/__install_scripts.sh
@@ -13,17 +13,17 @@
 #
 
 vmtools_root=/opt/vmtools
-vmtools_bin=$vmtools/bin
+vmtools_bin=$vmtools_root/bin
 vmtools_avail_vms=$vmtools_root/avail-vms
 vmtools_active_vms=$vmtools_root/active-vms
 
 ##############################################
 
 echo "Creating directories at $vmtools_root"
-mkdir -p $vmtools_root
-mkdir -p $vmtools_bin
-mkdir -p $vmtools_avail_vms
-mkdir -p $vmtools_active_vms
+mkdir -p --verbose $vmtools_root
+mkdir -p --verbose $vmtools_bin
+mkdir -p --verbose $vmtools_avail_vms
+mkdir -p --verbose $vmtools_active_vms
 
 cp -r ./install/opt/vmtools/bin/ $vmtools_bin/
 
@@ -31,7 +31,7 @@ cp -r ./install/opt/vmtools/bin/ $vmtools_bin/
 
 echo "Creating symlinks."
 cd $vmtools_bin
-for file in * ; do
+for file in ./* ; do
   if [[ -f $file ]]; then
     ln -s $file /usr/local/bin/`basename $file`
     echo "Link added: `basename $file`"
