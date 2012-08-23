47f86b5e81b6be2db16387e0d79b65e29ea383a4
Jeff McAffee <jeff@ktechsystems.com>
2012-08-22 19:29:17 -0700

    Moving globals, common funcs to bin/__vmtools.sh

--- install/__functions.sh
+++ install/__functions.sh
@@ -8,19 +8,13 @@
 # Website::   http://ktechsystems.com
 ##############################################################################
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/opt/vmtools/bin/__vmtools.sh
+
 ##############################################
 # Global Variables
 #
 
-VMTOOLS_ROOT=/opt/vmtools
-VMTOOLS_BIN=$VMTOOLS_ROOT/bin
-VMTOOLS_AVAIL_VMS=$VMTOOLS_ROOT/avail-vms
-VMTOOLS_ACTIVE_VMS=$VMTOOLS_ROOT/active-vms
-
-VMDK=""
-VMLIST=(core rails ec2sdk mysql)
-VMZIPS=(turnkey-core-12.0rc2-squeeze-x86-vmdk.zip turnkey-rails-11.3-lucid-x86-vmdk.zip turnkey-ec2sdk-11.3-lucid-x86-vmdk.zip turnkey-mysql-11.3-lucid-x86-vmdk.zip)
-TURNKEY="http://downloads.sourceforge.net/project/turnkeylinux/vmdk"
 
 
 ##############################################
@@ -74,58 +68,5 @@ function update_hostname () {
     return 0;
 }
 export -f update_hostname
-
-
-#---------------------------------------------
-# select_vmdk
-#
-# Allow user to select a VMDK.
-#
-# Selected VMDK will be returned in global $VMDK
-#
-# Parameters:
-#   default: default VMDK (use just hits <Enter>)
-#   msg: Message to display
-#   use_all: display 'all' option when 'Y'
-#
-function select_vmdk () {
-    local default="rails"
-    local msg="Select a VMDK:"
-    local use_all="N"
-
-    if [[ "X$1" != "X" ]]; then
-        default=$1
-    fi
-
-    if [[ "X$2" != "X" ]]; then
-        msg=$2
-    fi
-
-    if [[ "X$3" != "X" ]]; then
-        use_all=$3
-    fi
-
-    echo "$msg"
-    for vm in ${VMLIST[@]} ; do
-        echo "    $vm"
-    done
-
-    if [[ "$use_all" == "Y" ]]; then
-        echo "    all"
-    fi
-
-    echo ""
-    echo -n "Choice (default: $default)? "
-    read choice
-
-    if [[ "x$choice" != "x" ]]; then
-        VMDK=$choice
-    else
-        VMDK=$default
-    fi
-
-}
-export -f select_vmdk
-
 
 
